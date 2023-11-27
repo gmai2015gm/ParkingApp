e26ae0c0c31a5771f8d61e25cafe180fe4a2c6ed
Garrett <gmai2015gm@gmail.com>
2023-11-27 11:22:35 -0500

    Placed in all attributes for models.

--- SmartPark/app/src/main/java/com/example/smartpark/Models/ParkingLot.java
+++ SmartPark/app/src/main/java/com/example/smartpark/Models/ParkingLot.java
@@ -1,6 +1,14 @@
 package com.example.smartpark.Models;
 
+import java.util.ArrayList;
+
 public class ParkingLot
 {
-
+    public String name;
+    public float latitude;
+    public float longitude;
+    public double avgCleanliness;
+    public double avgSafety;
+    public double avgAvailability;
+    public ArrayList<Rating> ratings;
 }
--- SmartPark/app/src/main/java/com/example/smartpark/Models/Rating.java
+++ SmartPark/app/src/main/java/com/example/smartpark/Models/Rating.java
@@ -1,6 +1,15 @@
 package com.example.smartpark.Models;
 
+import java.time.LocalDateTime;
+
 public class Rating
 {
+    private User user; //Should this just be user name?
+    private ParkingLot parkingLot;
+    private int cleanliness;
+    private int safety;
+    private int availability;
+    private LocalDateTime timestamp;
+    private String notes;
 
 }
