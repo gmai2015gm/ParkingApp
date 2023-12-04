7b8b2af519dc63196a2b60ea50beca90c2942380
isaiahgalaviz07 <isaiahgalaviz07@gmail.com>
2023-12-03 20:11:00 -0500

    Update ParkingLotHelper.java

--- SmartPark/app/src/main/java/com/example/smartpark/APIHelpers/ParkingLotHelper.java
+++ SmartPark/app/src/main/java/com/example/smartpark/APIHelpers/ParkingLotHelper.java
@@ -16,6 +16,7 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
